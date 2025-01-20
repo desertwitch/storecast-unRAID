1460727a0b2833f5b1fa607665486e7b070b7acf
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-01-20 13:01:48 +0100

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cp -n /usr/local/emhttp/plugins/dwstorecast/storecast-def.json /tmp/storecast.json
+cp -n /usr/local/emhttp/plugins/dwstorecast/storecast-def.json /tmp/storecast-dash.json
 ln -sf /tmp/storecast.json /usr/local/emhttp/plugins/dwstorecast/storecast.json
 ln -sf /tmp/storecast-dash.json /usr/local/emhttp/plugins/dwstorecast/storecast-dash.json
