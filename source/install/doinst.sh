ac6890a85e643bc9bf0d2ab80b6f82e6cf64b110
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-01-20 12:32:27 +0100

    initial commit

---
+++ source/install/doinst.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -sf /tmp/storecast.json /usr/local/emhttp/plugins/dwstorecast/storecast.json
+ln -sf /tmp/storecast-dash.json /usr/local/emhttp/plugins/dwstorecast/storecast-dash.json
---
+++ source/usr/local/emhttp/plugins/dwstorecast/README.md
@@ -0,0 +1,3 @@
+**Storecast for UNRAID**
+
+Your personal weather forecast, just for future storage needs.
