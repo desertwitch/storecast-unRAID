f94010a266db21337de834b690656b68ca6bfb61
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-01-21 16:44:39 +0100

    changes

--- source/usr/local/emhttp/plugins/dwstorecast/include/dwstorecast_log.php
+++ source/usr/local/emhttp/plugins/dwstorecast/include/dwstorecast_log.php
@@ -22,28 +22,14 @@
         if(file_exists("/tmp/storecast.log")) {
             $modtime_log = filemtime("/tmp/storecast.log");
             if($modtime_log) $modtime_log = date("Y-m-d H:i:s", $modtime_log);
-
-            $modtime_json = false;
-            if(file_exists("/tmp/storecast.json")) {
-                $modtime_json = filemtime("/tmp/storecast.json");
-                if($modtime_json) $modtime_json = date("Y-m-d H:i:s", $modtime_json);
-            }
-            
             echo json_encode([
                 'running' => $storecast_running,
                 'modtime_log' => $modtime_log,
-                'modtime_json' => $modtime_json,
                 'response' => htmlspecialchars(file_get_contents("/tmp/storecast.log") ?: "Failed to load logfile.")
             ]);
         } else {
-            $modtime_json = false;
-            if(file_exists("/tmp/storecast.json")) {
-                $modtime_json = filemtime("/tmp/storecast.json");
-                if($modtime_json) $modtime_json = date("Y-m-d H:i:s", $modtime_json);
-            }
             echo json_encode([
                 'running' => $storecast_running,
-                'modtime_json' => $modtime_json,
                 'response' => "Waiting for Forecast..."
             ]);
         }
